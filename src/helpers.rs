db05c93b1d8aa57061b4f010282f3d1950f80f3c
Ryan Kurte <ryankurte@gmail.com>
2018-04-11 11:04:56 +1200

    PLaying with macros, currently broken

--- src/helpers.rs
+++ src/helpers.rs
@@ -16,48 +16,68 @@ macro_rules! read_masked {
     }
 }
 
+macro_rules! concat {
+    ($a: expr, $b: expr) => { $a$b };
+}
+
 // Generates a trait for a provided field type
 #[macro_export]
 macro_rules! field_trait {
-    (read, $name: ident, $field: tt, $t:ty, $shift: expr) => {
-        fn $name(&self) -> bool;
+    (r, $name: ident, $field: tt, $t:ty, $shift: expr) => {
+        fn read_$name(&self) -> bool;
     };
-    (write, $name: ident, $field: tt, $t:ty, $shift: expr) => {
-        fn $name(&mut self, v: bool);
+    (w, $name: ident, $field: tt, $t:ty, $shift: expr) => {
+        fn write_$name(&mut self, v: bool);
     };
-    (read, $name: ident, $field:tt, $t:ty, $shift:expr, $mask:expr) => {
-        fn $name(&self) -> $t;
+    (rw, $name: ident, $field: tt, $t:ty, $shift:expr) => {
+        field_trait!(r, $name, $field, $t, $shift);
+        field_trait!(w, $name, $field, $t, $shift);
     };
-    (write, $name: ident, $field:tt, $t:ty, $shift:expr, $mask:expr) => {
-        fn $name(&mut self, v: $t);
+    (r, $name: ident, $field:tt, $t:ty, $shift:expr, $mask:expr) => {
+        fn read_$name(&self) -> $t;
+    };
+    (w, $name: ident, $field:tt, $t:ty, $shift:expr, $mask:expr) => {
+        fn write_$name(&mut self, v: $t);
+    };
+    (rw, $name: ident, $field: tt, $t:ty, $shift:expr, $mask:expr) => {
+        field_trait!(r, $name, $field, $t, $shift, $mask);
+        field_trait!(w, $name, $field, $t, $shift, $mask);
     }
 }
 
 // Generates a method for a provided field type
 #[macro_export]
 macro_rules! field_method {
-    (read, $name: ident, $field: tt, $t:ty, $shift: expr) => {
-        fn $name(&self) -> bool {
+    (r, $name: ident, $field: tt, $t:ty, $shift: expr) => {
+        fn read_$name(&self) -> bool {
             self.$field & (1 << $shift) != 0
         }
     };
-    (write, $name: ident, $field: tt, $t:ty, $shift: expr) => {
-        fn $name(&mut self, v: bool) {
+    (w, $name: ident, $field: tt, $t:ty, $shift: expr) => {
+        fn write_$name(&mut self, v: bool) {
             self.$field = match v {
                 true => self.$field | (1 << $shift),
                 false => self.$field & !(1 << $shift),
             };
         }
     };
-    (read, $name: ident, $field: tt, $t:ty, $shift: expr, $mask: expr) => {
-        fn $name(&self) -> $t {
+    (rw, $name: ident, $field: tt, $t:ty, $shift:expr, $mask:expr) => {
+        field_method!(r, $name, $field, $t, $shift);
+        field_method!(w, $name, $field, $t, $shift);
+    };
+    (r, $name: ident, $field: tt, $t:ty, $shift: expr, $mask: expr) => {
+        fn read_$name(&self) -> $t {
             read_masked!(self.$field, $shift, $mask)
         }
     };
-    (write, $name: ident, $field: tt, $t:ty, $shift: expr, $mask: expr) => {
-        fn $name(&mut self, v: $t) {
+    (w, $name: ident, $field: tt, $t:ty, $shift: expr, $mask: expr) => {
+        fn write_$name(&mut self, v: $t) {
             write_masked!(self.$field, $shift, $mask, v);
         }
+    };
+    (rw, $name: ident, $field: tt, $t:ty, $shift:expr, $mask:expr) => {
+        field_method!(r, $name, $field, $t, $shift, $mask);
+        field_method!(w, $name, $field, $t, $shift, $mask);
     }
 }
 
@@ -82,10 +102,8 @@ mod tests {
 
     register!(TESTREG1, u16, 
         [
-            read,   read_bit1,  1,  bool,   1;
-            write,  write_bit1, 1,  bool,   1;
-            read,   read_var,   1,  u16,    2,  0b111;
-            write,  write_var,  1,  u16,    2,  0b111;
+            rw, bit1,  1,  bool,   1;
+            rw, var1,  1,  u16,    2,  0b111;
         ]
     );
 
@@ -99,9 +117,9 @@ mod tests {
         assert_eq!(true, r.read_bit1());
         assert_eq!(1 << 1, r.value());
 
-        assert_eq!(0, TESTREG1::read_var(&r));
-        TESTREG1::write_var(&mut r, 3);
-        assert_eq!(3, TESTREG1::read_var(&r));
+        assert_eq!(0, TESTREG1::read_var1(&r));
+        TESTREG1::write_var1(&mut r, 3);
+        assert_eq!(3, TESTREG1::read_var1(&r));
 
         assert_eq!(1 << 1 | 3 << 2, r.value());
     }
